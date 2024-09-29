ec5282f543c0663f3da34fd22de5d3d165611b92
Arshad vp <Arshad1071@yahoo.in>
2024-09-29 17:04:34 +0530

    first commit4

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT||5000
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.listen(PORT,()=>{
-    console.log('Hi');
-})
+const PORT = process.env.PORT || 5002;
+// const User = require('./model/User');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+app.use(cors()); // Enable CORS for all routes and origins
+
+app.use(
+  bodyParser.urlencoded(
+    { extended: false }
+  )
+)
+
+app.use(bodyParser.json())
+
+// const userRoute = require('./router/user');
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+// app.use('/api/user',userRoute)
+
+
+mongoose.connect(process.env.MONGODB_URL)
+
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
